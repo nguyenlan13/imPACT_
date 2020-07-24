b548e391af98bf2a7c90a3cb8c6159f040298c15
lanibon <nguyenlan13@gmail.com>
2020-07-23 20:46:14 -0700

    add steps attibutes

--- imPACT-backend/imPACT/db/migrate/20200723131050_create_steps.rb
+++ imPACT-backend/imPACT/db/migrate/20200723131050_create_steps.rb
@@ -1,8 +1,23 @@
 class CreateSteps < ActiveRecord::Migration[6.0]
-  def change
-    create_table :steps do |t|
-
-      t.timestamps
+    def change
+        create_table :steps do |t|
+            t.integer :user_id
+            t.integer :habit_id
+            t.string :action_step
+            t.boolean :before, default: true
+            t.string :start_datetime
+            t.string :location
+            t.time :action_time
+            t.boolean :monday, default: false
+            t.boolean :tuesday, default: false
+            t.boolean :wednesday, default: false
+            t.boolean :thursday, default: false
+            t.boolean :friday, default: false
+            t.boolean :saturday, default: false
+            t.boolean :sunday, default: false
+            t.integer :duration_number
+            t.string :duration_type
+            t.timestamps
+        end
     end
-  end
 end
