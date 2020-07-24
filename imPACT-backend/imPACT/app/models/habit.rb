188319522820df6ea95d24efd50601db2d085bab
lanibon <nguyenlan13@gmail.com>
2020-07-23 20:05:49 -0700

    add habit attributes

--- imPACT-backend/imPACT/app/models/habit.rb
+++ imPACT-backend/imPACT/app/models/habit.rb
@@ -1,11 +1,3 @@
 class Habit < ApplicationRecord
-    def change
-        create_table :habits do |t|
-            t.boolean :build, default: true
-            t.string :title
-            t.integer :frequency_number
-            t.string :frequency
-            t.timestamps
-        end
 
 end
--- imPACT-backend/imPACT/db/migrate/20200720000124_create_habits.rb
+++ imPACT-backend/imPACT/db/migrate/20200720000124_create_habits.rb
@@ -1,8 +1,11 @@
 class CreateHabits < ActiveRecord::Migration[6.0]
-  def change
-    create_table :habits do |t|
-
-      t.timestamps
+    def change
+        create_table :habits do |t|
+            t.boolean :build, default: true
+            t.string :title
+            t.integer :frequency_number
+            t.string :frequency
+            t.timestamps
+        end
     end
-  end
 end
