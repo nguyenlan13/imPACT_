09ea6dd59590b4d14c76d33e73584521db21cbe2
lanibon <nguyenlan13@gmail.com>
2020-07-23 22:25:44 -0700

    add comments attributes

--- imPACT-backend/imPACT/db/migrate/20200723131121_create_comments.rb
+++ imPACT-backend/imPACT/db/migrate/20200723131121_create_comments.rb
@@ -1,8 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.0]
-  def change
-    create_table :comments do |t|
-
-      t.timestamps
+    def change
+        create_table :comments do |t|
+            t.integer :user_id
+            t.text :content
+            t.references :commentable, polymorphic: true, index: true
+            t.timestamps
+        end
     end
-  end
 end
