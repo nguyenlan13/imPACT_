182eb0262ebdefedae1c2ccb94fe18722f24fd56
lanibon <nguyenlan13@gmail.com>
2020-07-23 22:25:17 -0700

    add streaks attributes

--- imPACT-backend/imPACT/db/migrate/20200723131112_create_streaks.rb
+++ imPACT-backend/imPACT/db/migrate/20200723131112_create_streaks.rb
@@ -1,8 +1,13 @@
 class CreateStreaks < ActiveRecord::Migration[6.0]
-  def change
-    create_table :streaks do |t|
-
-      t.timestamps
+    def change
+        create_table :streaks do |t|
+            t.integer :habit_id
+            t.integer :user_id
+            t.integer :current_streak
+            t.integer :longest_streak
+            t.datetime :longest_streak_end_date
+            t.timestamps
+        end
     end
-  end
 end
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
