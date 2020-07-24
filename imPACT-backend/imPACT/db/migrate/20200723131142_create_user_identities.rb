a68d176ae9d3a9d92910ca3aa43b58c086a6ce8c
lanibon <nguyenlan13@gmail.com>
2020-07-23 22:26:34 -0700

    add user identities attributes

--- imPACT-backend/imPACT/db/migrate/20200723131142_create_user_identities.rb
+++ imPACT-backend/imPACT/db/migrate/20200723131142_create_user_identities.rb
@@ -1,8 +1,9 @@
 class CreateUserIdentities < ActiveRecord::Migration[6.0]
-  def change
-    create_table :user_identities do |t|
-
-      t.timestamps
+    def change
+        create_table :user_identities do |t|
+            t.integer :user_id
+            t.integer :identity_id
+            t.timestamps
+        end
     end
-  end
 end
