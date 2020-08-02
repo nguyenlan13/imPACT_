36c746bc075c0c45a6cee2b6da6205e18e42049e
lanibon <nguyenlan13@gmail.com>
2020-08-02 16:46:46 -0700

    add habit associations

--- imPACT-backend/imPACT/app/models/habit.rb
+++ imPACT-backend/imPACT/app/models/habit.rb
@@ -1,3 +1,25 @@
 class Habit < ApplicationRecord
+    # validates :build, presence: true
+    validates :title, presence: true
+    validates :frequency_number, presence: true, numericality: true
+    validates :frequency, presence: true
+    
+    
+    
+    has_many :streaks, dependent: :destroy
+    has_many :steps, dependent: :destroy
+    has_many :identity_habits, dependent: :destroy
+    has_many :identities, through: :identity_habits
+    has_many :users, through: :identities
+    has_many :comments, as: :commentable, dependent: :destroy
 
+    #scope method
+
+    def self.building
+        where(build: true).order(created_at: :desc).limit(5)
+    end
+
+    def self.quit
+        where(build: false).order(created_at: :desc).limit(5)
+    end
 end
