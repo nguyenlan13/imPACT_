15656f6f29057f24f1bba7d85add8ee4a8e1a61c
lanibon <nguyenlan13@gmail.com>
2020-08-02 16:41:18 -0700

    add comment associations

--- imPACT-backend/imPACT/app/models/comment.rb
+++ imPACT-backend/imPACT/app/models/comment.rb
@@ -1,2 +1,13 @@
 class Comment < ApplicationRecord
+
+    validates :user_id, presence: true
+    validates :content, presence: true
+    validates :commentable_id, presence: true
+    validates :commentable_type, presence: true
+  
+    belongs_to :user
+    belongs_to :commentable, polymorphic: true
+
+    has_many :comments, as: :commentable, dependent: :destroy
+    has_many :reactions, as: :reactable, dependent: :destroy
 end
