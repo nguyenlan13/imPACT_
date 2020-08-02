d11d12ab5d2ef0b12e46f60a90eb183ae653cd0d
lanibon <nguyenlan13@gmail.com>
2020-08-02 16:49:53 -0700

    add identity associations

--- imPACT-backend/imPACT/app/models/identity.rb
+++ imPACT-backend/imPACT/app/models/identity.rb
@@ -1,2 +1,10 @@
 class Identity < ApplicationRecord
+    has_many :user_identities, dependent: :destroy
+    has_many :users, through: :user_identities
+    has_many :identity_habits, dependent: :destroy
+    has_many :habits, through: :identity_habits
+    has_many :comments, as: :commentable, dependent: :destroy
+
+    validates :pact_name, uniqueness: true, presence: true
+    validates :description, uniqueness: true, presence: true
 end
