2464b64ee008425daf070474bab4fa493563ed22
lanibon <nguyenlan13@gmail.com>
2020-08-06 06:44:10 -0700

    add reaction associations

--- imPACT-backend/imPACT/app/models/reaction.rb
+++ imPACT-backend/imPACT/app/models/reaction.rb
@@ -1,2 +1,10 @@
 class Reaction < ApplicationRecord
+    validates :user_id, presence: true
+    validates :reactable_id, presence: true
+    validates :reactable_type, presence: true
+    validates :reaction_type, presence: true
+  
+    belongs_to :user
+    belongs_to :reactable, polymorphic: true
+
 end
