bb108259d182c7acb3d13fcbaa6fe4fe7ff0f0f9
lanibon <nguyenlan13@gmail.com>
2020-08-06 06:36:26 -0700

    add dashboard container

---
+++ imPACT-frontend/impact/src/containers/dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'React'
+
+class Dashboard extends Component {
+
+    render(){
+        
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default Dashboard
