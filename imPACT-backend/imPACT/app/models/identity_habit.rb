b67e5858d190d885f952ea8485528e5489a94d12
lanibon <nguyenlan13@gmail.com>
2020-08-06 06:43:44 -0700

    add identity_habits associations

--- imPACT-backend/imPACT/app/models/identity_habit.rb
+++ imPACT-backend/imPACT/app/models/identity_habit.rb
@@ -1,2 +1,9 @@
 class IdentityHabit < ApplicationRecord
+    validates :identity_id, presence: true
+    validates :habit_id, presence: true
+    validates_uniqueness_of :identity_id, :scope => :habit_id
+
+    belongs_to :identity
+    belongs_to :habit
+
 end
