87eaa05d2d3cd9a4512b92e7b38e2998b84a3079
lanibon <nguyenlan13@gmail.com>
2020-08-06 06:44:30 -0700

    add step associations

--- imPACT-backend/imPACT/app/models/step.rb
+++ imPACT-backend/imPACT/app/models/step.rb
@@ -1,2 +1,15 @@
 class Step < ApplicationRecord
+    validates :user_id, presence: true
+    validates :habit_id, presence: true
+    validates :action_step, presence: true
+    validates :start_datetime, presence: true
+    validates :location, presence: true
+    validates :action_time, presence: true
+    validates :duration_number, presence: true
+    validates :duration_type, presence: true
+  
+  
+    belongs_to :user
+    belongs_to :habit
+
 end
