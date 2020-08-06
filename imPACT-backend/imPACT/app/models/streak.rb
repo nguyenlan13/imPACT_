7cb777984db68d71261bdaccd0e023d38b4b18d1
lanibon <nguyenlan13@gmail.com>
2020-08-06 06:44:52 -0700

    add streak associations

--- imPACT-backend/imPACT/app/models/streak.rb
+++ imPACT-backend/imPACT/app/models/streak.rb
@@ -1,2 +1,8 @@
 class Streak < ApplicationRecord
+    belongs_to :habit
+    belongs_to :user
+  
+    has_many :comments, as: :commentable, dependent: :destroy
+    has_many :reactions, as: :reactable, dependent: :destroy
+
 end
