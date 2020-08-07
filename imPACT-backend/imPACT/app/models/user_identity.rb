5842db2c77adf658d6ed4790810506ec23151759
lanibon <nguyenlan13@gmail.com>
2020-08-06 20:56:42 -0700

    add user_identity associations

--- imPACT-backend/imPACT/app/models/user_identity.rb
+++ imPACT-backend/imPACT/app/models/user_identity.rb
@@ -1,2 +1,8 @@
 class UserIdentity < ApplicationRecord
+
+    validates :user_id, presence: true
+    validates :identity_id, presence: true
+    validates_uniqueness_of :user_id, :scope => :identity_id
+    belongs_to :user
+    belongs_to :identity
 end
