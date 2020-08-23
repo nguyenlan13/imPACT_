7e8a9270d0039c63bb607da7dc7f5e8e667e1343
lanibon <nguyenlan13@gmail.com>
2020-08-22 20:08:02 -0700

    add application controller methods

--- imPACT-backend/imPACT/app/controllers/application_controller.rb
+++ imPACT-backend/imPACT/app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::API
 
-    
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
+    end 
     
 end
