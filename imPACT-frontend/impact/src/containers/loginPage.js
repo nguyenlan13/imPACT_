44d52a1fcdcdb8f13f240a9449261face9672ae5
lanibon <nguyenlan13@gmail.com>
2020-08-22 23:19:07 -0700

    add login page

---
+++ imPACT-frontend/impact/src/containers/loginPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'React'
+
+class loginPage extends Component {
+
+    render(){
+        
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default loginPage
