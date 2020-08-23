ceb12869a4a0f6cd2052388d15cd7f9583ebfea6
lanibon <nguyenlan13@gmail.com>
2020-08-22 23:19:19 -0700

    add signup page

---
+++ imPACT-frontend/impact/src/containers/signupPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'React'
+
+class signupPage extends Component {
+
+    render(){
+        
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default signupPage
