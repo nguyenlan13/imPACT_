c556e610694aadc53f5e95d33577de854437452c
lanibon <nguyenlan13@gmail.com>
2020-08-29 21:04:27 -0700

    add comments controller

---
+++ imPACT-backend/imPACT/app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+    def index
+        current_user
+        if params[:comment_id]
+            comment = Comment.find(params[:comment_id]).comments
+        elsif params[:habit_id]
+            comment = Habit.find(params[:habit_id]).comments
+        elsif params[:identity_id]
+            comment = Identity.find(params[:identity_id]).comments
+        elsif params[:streak_id]
+            comment = Streak.find(params[:streak_id]).comments
+        else
+            comments = Comment.all
+        end
+            render json: comments
+    end 
+
+
+
+end
