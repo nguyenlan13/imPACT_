227f9a5f90ee6d34da83c07e5b253eed318a1770
lanibon <nguyenlan13@gmail.com>
2020-09-02 18:55:17 -0700

    add cookie info

--- imPACT-backend/imPACT/app/controllers/application_controller.rb
+++ imPACT-backend/imPACT/app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
+    
+    protect_from_forgery with: :exception
+    before_action :set_csrf_cookie
 
+    
     def log_in(user)
         session[:user_id] = user.id
     end
@@ -12,4 +18,11 @@ class ApplicationController < ActionController::API
         !!current_user
     end 
     
+    private
+
+    def set_csrf_cookie
+        cookies["CSRF-TOKEN"] = form_authenticity_token
+    end
+
+
 end
