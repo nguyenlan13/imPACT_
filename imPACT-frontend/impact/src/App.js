c69d422cbfa2c0930d6611b4681074ed922022b0
lanibon <nguyenlan13@gmail.com>
2020-09-15 20:52:34 -0700

    add routes

--- imPACT-frontend/impact/src/App.js
+++ imPACT-frontend/impact/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './containers/dashboard'
+import Profile from './containers/profilePage'
+import Login from './containers/loginPage'
+import Signup from './containers/signupPage'
 
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -10,6 +14,10 @@ function App() {
       <Router>
         <div className="App">
             <Switch>
+                <Route exact path="/dashboard" component={Dashboard}/>
+                <Route exact path="/profile" component={Profile}/>
+                <Route exact path="/login" component={Login}/>
+                <Route exact path="/signup" component={Signup}/>
     
             </Switch>
         </div>
