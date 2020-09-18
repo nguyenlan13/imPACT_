98523e70443a9a25f51cbd8b4e572f1125baee51
lanibon <nguyenlan13@gmail.com>
2020-09-17 21:26:45 -0700

    add auth route

--- imPACT-backend/imPACT/config/routes.rb
+++ imPACT-backend/imPACT/config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/auth" => 'sessions#auth'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
