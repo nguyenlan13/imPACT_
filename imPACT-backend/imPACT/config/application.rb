2d54a7fef5d69124dbd11776e36a3783e145f941
lanibon <nguyenlan13@gmail.com>
2020-09-17 21:09:14 -0700

    add cookies

--- imPACT-backend/imPACT/config/application.rb
+++ imPACT-backend/imPACT/config/application.rb
@@ -9,12 +9,15 @@ Bundler.require(*Rails.groups)
 module ImPACT
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.api_only = true
+    
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
