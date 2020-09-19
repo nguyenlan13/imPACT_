bbb4a6640f895a9524f1d1484af867a6ef95cba3
lanibon <nguyenlan13@gmail.com>
2020-09-18 23:03:48 -0700

    add user

---
+++ imPACT-backend/imPACT/app/controllers/user_controller.rb
@@ -0,0 +1,29 @@
+class UserController < ApplicationController
+
+    def new
+        user = User.new
+        render json: user, status: 200
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            log_in(user)
+            cookies["logged_in"] = true
+            render json: user, except [:password_digest]
+        else
+            render json: {errors: user.errors.full_messages}, status: 400
+        end
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user, status: 200
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:email, :name, :username, :password)
+    end
+end
--- imPACT-frontend/impact/src/actions/user.js
+++ imPACT-frontend/impact/src/actions/user.js
@@ -2,3 +2,34 @@ import { LOG_IN_USER } from '../actionTypes'
 import { SIGN_UP_USER } from '../actionTypes'
 import { LOGOUT } from '../actionTypes'
 
+export const login = (csrf_token, email, password) => {
+    return async function (dispatch) {
+        try{
+            const res = await fetch("http://localhost:3001/login", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': csrf_token
+                },
+                body: JSON.stringify({email, password}),
+                credentials: 'include'
+            })
+            if(!res.ok){
+                throw res
+            }
+            let loginJson = await res.json()
+            dispatch({
+                type: LOG_IN_USER,
+                payload: loginJson
+                // {
+                //         email: email,
+                //         password: password
+                // }
+            });
+            // return await res.json()
+        }catch(error){
+            console.log(error.message)
+        }
+    }
+}
