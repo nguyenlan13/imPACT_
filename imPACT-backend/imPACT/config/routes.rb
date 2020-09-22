cda422711a3b1ac35b1ed4461359439545c9d530
lanibon <nguyenlan13@gmail.com>
2020-09-21 21:57:55 -0700

    add user routes

--- imPACT-backend/imPACT/config/routes.rb
+++ imPACT-backend/imPACT/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/auth" => 'sessions#auth'
+    get "/auth" => 'sessions#auth'
 
-
+    resources :users do
+        resources :habits
+    end
 
 
 
