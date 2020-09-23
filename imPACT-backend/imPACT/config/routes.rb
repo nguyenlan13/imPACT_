a39f653f9cdc5821476c8590a4bece254b10398a
lanibon <nguyenlan13@gmail.com>
2020-09-23 06:47:22 -0700

    add login

--- imPACT-backend/imPACT/config/routes.rb
+++ imPACT-backend/imPACT/config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
     get "/auth" => 'sessions#auth'
+    post "/login" => 'sessions#create'
+
 
     resources :users do
         resources :habits
     end
 
-
+    
 
 
 
--- imPACT-frontend/impact/src/containers/loginPage.js
+++ imPACT-frontend/impact/src/containers/loginPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import LoginForm from '../components/login/loginForm'
 import { getToken } from '../actions/authSetup'
+import { login } from '../actions/user'
 
 class loginPage extends Component {
 
@@ -10,6 +11,7 @@ class loginPage extends Component {
      }
 
     submitHandler = async (email, password) => {
+        await this.props.login(this.props.csrf_token, email, password)
         
     }
 
@@ -18,7 +20,7 @@ class loginPage extends Component {
         
         return(
             <div>
-
+                <LoginForm handleSubmit={this.submitHandler}/>
             </div>
         )
     }
@@ -30,7 +32,8 @@ const mapStateToProps = ({csrf_token}) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    get_token: () => dispatch(getToken())
+    get_token: () => dispatch(getToken()),
+    login: (csrf_token, email, password) => dispatch(login(csrf_token, email, password))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(loginPage)
