c9ca70f09cb1cb5436d6fd326b67b45183f3b2c6
lanibon <nguyenlan13@gmail.com>
2020-09-23 19:06:35 -0700

    add signup

--- imPACT-frontend/impact/src/actions/user.js
+++ imPACT-frontend/impact/src/actions/user.js
@@ -33,3 +33,40 @@ export const login = (csrf_token, email, password) => {
         }
     }
 }
+
+
+
+export const signup = (csrf_token, email, username, name, password) => {
+    return async function (dispatch) {
+        try{
+            const res = await fetch("http://localhost:3001/signup", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': csrf_token
+                },
+                body: JSON.stringify({user:{email, username, name, password}}),
+                credentials: 'include'
+            })
+            // if(!res.ok){
+            //     throw res
+            // }
+            let signupJson = await res.json()
+            if(!Object.keys(signupJson).includes("errors")){
+                dispatch({
+                    type: SIGN_UP_USER,
+                    payload: signupJson
+                // {
+                //         email: email,
+                //         password: password
+                // }
+            });
+        }
+        return signupJson
+            // return await res.json()
+        }catch(error){
+            console.log(error.message)
+        }
+    }
+}
