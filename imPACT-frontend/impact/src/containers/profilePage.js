efa179a4e0990b07ad3eb94f74a69e938dc6daa1
lanibon <nguyenlan13@gmail.com>
2020-09-24 22:35:13 -0700

    add header

--- imPACT-frontend/impact/src/containers/profilePage.js
+++ imPACT-frontend/impact/src/containers/profilePage.js
@@ -5,8 +5,8 @@ class Profile extends Component {
     render(){
         
         return(
-            <div>
-
+            <div className="page">
+                <h1>PROFILE</h1>
             </div>
         )
     }
