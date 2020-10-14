85784772747c5c93736cf35776cad9d915ce451f
lanibon <nguyenlan13@gmail.com>
2020-10-14 00:49:26 -0700

    add habit action

---
+++ imPACT-frontend/impact/src/actions/habit.js
@@ -0,0 +1,25 @@
+import { GET_ALL_HABITS,
+        GET_MY_HABITS,
+        ADD_HABIT
+    } from '../actionTypes'
+
+export const getAllHabits = () => {
+    return async function (dispatch) {
+        try{
+            const res = await fetch("http://localhost:3001/habits", {
+                credentials: 'include'
+            })
+            if(!res.ok){
+                throw res
+            }
+            const habitJson = await res.json()
+            dispatch({
+                type: GET_ALL_HABITS,
+                payload: habitJson
+            })
+        }catch(error){
+            console.log(error)
+        }
+    }
+}
+
