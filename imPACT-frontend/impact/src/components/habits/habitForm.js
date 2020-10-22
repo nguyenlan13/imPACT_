eb7abdb51a06cfd4e56a5c1828321581a09f5437
lanibon <nguyenlan13@gmail.com>
2020-10-22 06:52:52 -0700

    add habitform

--- imPACT-frontend/impact/src/components/habits/habitForm.js
+++ imPACT-frontend/impact/src/components/habits/habitForm.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 
 const HabitForm = (props) => {
- const [name, setName] = useState("")
- const handleSubmit = event => {
-     event.preventDefault()
+    const [name, setName] = useState("")
+    const handleSubmit = event => {
+        event.preventDefault()
 
-     props.handleSubmit(name)
+        props.handleSubmit(name)
  }
+
 return (
     <form onSubmit={handleSubmit}>
-
+     <input
+        className="mr-sm-2"
+        type="text"
+        placeholder="Name"
+        onChange={event => setName(event.target.value)}
+        value={name}
+      />
+      <br/>
+      <input type="submit" />
     </form>
 )
 
