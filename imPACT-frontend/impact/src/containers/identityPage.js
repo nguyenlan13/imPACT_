59776c273a79a8952bfe019b76f712c16f1dd32e
lanibon <nguyenlan13@gmail.com>
2020-10-29 06:38:49 -0700

    add identity form on page

--- imPACT-frontend/impact/src/containers/identityPage.js
+++ imPACT-frontend/impact/src/containers/identityPage.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import IdentityForm from '../components/identities/identityForm'
+// import { addIdentity } from '../actions/identity'
 
 class identityPage extends Component {
 
+    submitHandler = async (name) => {
+        await this.props.addIdentity(this.props.csrf_token, name)
+        // this.props.history.push("/profile")
+    }
+
+
     render(){
         return(
-            <div>
-                
+            <div className="page">
+                <h1>Add an Identity:</h1>
+                {/* <IdentityForm/> */}
+                <IdentityForm handleSubmit={this.submitHandler}/>
             </div>
         )
     }
 }
 
-export default identityPage
+const mapStateToProps = ({csrf_token}) => ({
+    csrf_token
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    // add_dentity: (csrf_token, name) => dispatch(addIdentity(csrf_token, name))
+})
+
+// export default identityPage
+
+export default connect(mapStateToProps, mapDispatchToProps)(identityPage)
+
+
+
+
