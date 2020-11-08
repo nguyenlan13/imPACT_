06acf704ce7281b1fa82e87dbd65e557cc9a39a2
lanibon <nguyenlan13@gmail.com>
2020-11-08 15:16:53 -0800

    add new action

--- imPACT-frontend/impact/src/actions/identity.js
+++ imPACT-frontend/impact/src/actions/identity.js
@@ -3,3 +3,23 @@ import { GET_ALL_IDENTITIES,
     ADD_IDENTITY,
     JOIN_IDENTITY
 } from '../actionTypes'
+
+export const getAllHabits = () => {
+    return async function (dispatch) {
+        try{
+            const res = await fetch("http://localhost:3001/habits", {
+                credentials: 'include'
+            })
+            if(!res.ok){
+                throw res
+            }
+            const habitJson = await res.json()
+            dispatch({
+                type: GET_ALL_IDENTITIES,
+                payload: habitJson
+            })
+        }catch(error){
+            console.log(error)
+        }
+    }
+}
