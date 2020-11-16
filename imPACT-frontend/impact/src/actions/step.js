d47ccccb48b57918021e2593dd18e7b0a102713f
lanibon <nguyenlan13@gmail.com>
2020-11-15 21:02:21 -0800

    add step action

---
+++ imPACT-frontend/impact/src/actions/step.js
@@ -0,0 +1,30 @@
+import { ADD_STEP
+} from '../actionTypes'
+
+export const addStep = (csrf_token, name, stepId) => {
+    return async function (dispatch) {
+        try{
+            let response = await fetch(`http://localhost:3001/habits/${stepId}`,{
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': csrf_token
+                },
+                body: JSON.stringify({step: {name}}),
+                credentials: 'include'
+            })
+            if(!response.ok){
+                throw response
+            }
+            let stepJson = await response.json()
+            dispatch({
+                type: ADD_STEP,
+                payload: 
+                    stepJson
+            })
+        }catch(error){
+            console.log(error.message)
+        }
+    }
+}
