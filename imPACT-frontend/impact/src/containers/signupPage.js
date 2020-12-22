c1df9535d8cb4ab2f1d6d4c7ef72324de391532b
lanibon <nguyenlan13@gmail.com>
2020-12-21 23:09:20 -0800

    add signup page

--- imPACT-frontend/impact/src/containers/signupPage.js
+++ imPACT-frontend/impact/src/containers/signupPage.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import SignupForm from '../components/signup/signupForm'
+import { getToken } from '../actions/authSetup'
+import { signup } from '../actions/user'
+
 
 class signupPage extends Component {
+
+    componentDidMount(){
+        this.props.get_token()
+    }
+
+    submitHandler = async (email, username, name, password) => {
+        await this.props.signup(this.props.csrf_token, email, username, name, password)
+        this.props.history.push("/profile")
+    }
 
     render(){
         
         return(
-            <div>
-
+            <div className="page">
+            <h1>SIGN UP:</h1>
+            <SignupForm handleSubmit={this.submitHandler}/>
             </div>
         )
     }
-
 }
 
-export default signupPage
+const mapStateToProps = ({csrf_token}) => ({
+    csrf_token
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    get_token: () => dispatch(getToken()),
+    signup: (csrf_token, email, username, name, password) => dispatch(signup(csrf_token, email, username, name, password))
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(signupPage)
+
