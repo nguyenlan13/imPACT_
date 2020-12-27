93805d5eaea8c1b3f38993ab3cc50a5284e2a110
lanibon <nguyenlan13@gmail.com>
2020-12-27 00:14:49 -0800

    add habit form

--- imPACT-frontend/impact/src/containers/habitPage.js
+++ imPACT-frontend/impact/src/containers/habitPage.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import HabitForm from '../components/habits/habitForm'
 
 
 class habitPage extends Component {
 
+
+    submitHandler = async (name) => {
+        await this.props.addHabit(this.props.csrf_token, name)
+        // this.props.history.push("/profile")
+    }
     render() {
         return(
-            <div>
-                
+            <div className="page">
+            <h1>Add a New Habit:</h1>
+            <HabitForm handleSubmit={this.submitHandler}/>
             </div>
             
         )
@@ -17,4 +24,16 @@ class habitPage extends Component {
     
 }
 
-export default habitPage
+
+const mapStateToProps = ({csrf_token}) => ({
+    csrf_token
+    })
+    
+    const mapDispatchToProps = (dispatch) => ({
+    // add_habit: (csrf_token, name) => dispatch(addHabit(csrf_token, name))
+    })
+export default connect(mapStateToProps, mapDispatchToProps)(habitPage)
+
+
+
+
