4dacdfd92b1d431de7b32a565055cc9f0080207b
lanibon <nguyenlan13@gmail.com>
2020-12-28 23:25:31 -0800

    change spacing

--- imPACT-frontend/impact/src/containers/profilePage.js
+++ imPACT-frontend/impact/src/containers/profilePage.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 
+
 class Profile extends Component {
+
+    componentDidMount(){
+        
+    }
 
     render(){
         
         return(
             <div className="page">
                 <h1>PROFILE</h1>
+
+
             </div>
         )
     }
