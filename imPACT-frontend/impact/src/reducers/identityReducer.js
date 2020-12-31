67b0d0bc0487480bfdae3f322509dd24eac0f8bd
lanibon <nguyenlan13@gmail.com>
2020-12-30 21:10:20 -0800

    add reducers

--- imPACT-frontend/impact/src/reducers/identityReducer.js
+++ imPACT-frontend/impact/src/reducers/identityReducer.js
@@ -3,3 +3,21 @@ import {
     GET_MY_IDENTITIES,
     ADD_IDENTITY,
 } from '../actionTypes'
+
+export default function identityReducer(state = {
+    identities:[],
+    myIdentities:[],
+
+}, action){
+
+    switch(action.type){
+        case GET_ALL_IDENTITIES:
+            return {...state, identities: action.payload}
+        case GET_MY_IDENTITIES:
+            return {...state, myIdentities: action.payload}
+        case ADD_IDENTITY:
+            return {...state, Identities:[...state.identities, action.payload]}      
+        default:
+            return state
+    }
+}
--- imPACT-frontend/impact/src/reducers/rootReducer.js
+++ imPACT-frontend/impact/src/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import tokenReducer from './tokenReducer'
 import identityReducer from './identityReducer'
+import habitReducer from './habitReducer'
 
 
 const rootReducer = (combineReducers) ({
-    csrf_token: tokenReducer
-    // identity: identityReducer,
+    csrf_token: tokenReducer,
+    identities: identityReducer,
+    habits: habitReducer
 
 
 })
