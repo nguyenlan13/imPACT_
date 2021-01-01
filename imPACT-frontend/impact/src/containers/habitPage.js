72d27928daf336a97ad02a803f87d04a794af895
lanibon <nguyenlan13@gmail.com>
2020-12-31 22:10:57 -0800

    add get request

--- imPACT-frontend/impact/src/containers/habitPage.js
+++ imPACT-frontend/impact/src/containers/habitPage.js
@@ -3,37 +3,55 @@ import { connect } from 'react-redux'
 import HabitForm from '../components/habits/habitForm'
 import { getAllHabits } from '../actions/habit'
 import { addHabit } from '../actions/habit'
+import HabitItem from '../components/habits/habitItem'
 
 class habitPage extends Component {
 
-
-    submitHandler = async (build, title, number, frequency) => {
-        await this.props.add_habit(this.props.csrf_token, build, title, number, frequency)
-        // this.props.history.push("/profile")
+    componentDidMount(){
+        this.props.get_all_habits()
     }
-    
+
+    submitHandler = async (build, title, frequency_number, frequency) => {
+        await this.props.add_habit(this.props.csrf_token, build, title, frequency_number, frequency)
+        this.props.history.push("/profile")
+    }
+
     render() {
+        const { habits } = this.props
+        const sortedHabits = habits.sort(function(a,b){
+            if(a.title < b.title) {return -1;}
+            if(a.title > b.title) {return 1;}
+            return 0
+        })    
         return(
             <div className="page">
             <h1>Add a New Habit:</h1>
             <HabitForm handleSubmit={this.submitHandler}/>
+            {sortedHabits.map(habit => {
+                    return <HabitItem
+                    title={habit.title}
+                    frequency_number={habit.frequency_number}
+                    // frequency={habit.frequency}
+                    />
+                })}
             </div>
             
         )
     }
-       
-
-    
 }
 
 
-const mapStateToProps = ({csrf_token}) => ({
-    csrf_token
-})
+const mapStateToProps = (state) => {
+    const { csrf_token, habits } = state
+    return {
+        csrf_token: csrf_token,
+        habits: habits.habits
+    }
+ }
     
 const mapDispatchToProps = (dispatch) => ({
     get_all_habits: () => dispatch(getAllHabits()),
-    add_habit: (csrf_token, build, title, number, frequency) => dispatch(addHabit(csrf_token, build, title, number, frequency))
+    add_habit: (csrf_token, build, title, frequency_number, frequency) => dispatch(addHabit(csrf_token, build, title, frequency_number, frequency))
 })
 
 
--- imPACT-frontend/impact/src/containers/identityPage.js
+++ imPACT-frontend/impact/src/containers/identityPage.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import IdentityForm from '../components/identities/identityForm'
-import { addIdentity } from '../actions/identity'
+import { addIdentity, getAllIdentities } from '../actions/identity'
+import IdentityItem from '../components/identities/identityItem'
 
 class identityPage extends Component {
+
+    componentDidMount(){
+        this.props.get_all_identities()
+    }
 
     submitHandler = async (pact_name, description) => {
         await this.props.add_identity(this.props.csrf_token, pact_name, description)
@@ -12,10 +17,24 @@ class identityPage extends Component {
 
 
     render(){
+        const { identities } = this.props
+        const sortedIdentities = identities.sort(function(a,b){
+            if(a.pact_name < b.pact_name) {return -1;}
+            if(a.pact_name > b.pact_name) {return 1;}
+            return 0
+        })
         return(
             <div className="page">
                 <h1>Add an Identity:</h1>
                 <IdentityForm handleSubmit={this.submitHandler}/>
+                {sortedIdentities.map(identity => {
+                    return <IdentityItem
+                    pact_name={identity.pact_name}
+                    description={identity.description}
+                    
+                    />
+                })}
+                
             </div>
         )
     }
@@ -30,6 +49,7 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
+    get_all_identities: () => dispatch(getAllIdentities()),
     add_identity: (csrf_token, pact_name, description) => dispatch(addIdentity(csrf_token, pact_name, description))
 })
 
